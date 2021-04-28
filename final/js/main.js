08ab09ee5aaa4c96cbde85ede566c28b349a5ac5
ralfz123 <ralf_zonneveld@hotmail.com>
2021-04-28 12:04:15 +0200

    darkmode + folder structure + UI bttns

--- final/js/main.js
+++ final/js/main.js
@@ -2,27 +2,43 @@ let video = document.querySelector('video');
 let playBtn = document.querySelector('.play-btn');
 let pauseBtn = document.querySelector('.pause-btn');
 
-playBtn.addEventListener('click', function () {
+function playVid() {
   video.play();
-  video.removeAttribute('controls');
-});
+  // video.removeAttribute('controls');
+
+  let body = document.querySelector('body main div');
+  let container = document.createElement('div');
+  body.appendChild(container);
+
+  setTimeout(function () {
+    container.style.display = 'none';
+  }, 39500);
+
+  setTimeout(function () {
+    container.style.display = 'block';
+  }, 42000);
+}
+
+// playBtn.addEventListener('click', function () {
+
+// });
 
 pauseBtn.addEventListener('click', function () {
   video.pause();
-  video.setAttribute('controls', 'controls');
+  // video.setAttribute('controls', 'controls');
 });
 
-// setTimeout(function () {
-//   let body = document.querySelector('body main div');
-//   let img = document.createElement('img');
-//   img.setAttribute(
-//     'style',
-//     'position:absolute;bottom:12px;display:block;width:50px;right:0;background:#ff000087;clip-path: circle(50% at 50% 50%);'
-//   );
-//   img.setAttribute(
-//     'src',
-//     'https://static.vecteezy.com/system/resources/previews/001/200/798/non_2x/music-note-png.png'
-//   );
-
-//   body.appendChild(img);
-// }, 1000);
+function darkModeToggle() {
+  var element = document.body;
+  element.classList.toggle('dark-mode');
+  let linkHeader = document.querySelector('header a');
+  linkHeader.classList.toggle('dark-mode');
+  let linkFooter = document.querySelector('footer a');
+  linkFooter.classList.toggle('dark-mode');
+  // let playBtn = document.querySelector('.play-btn');
+  // let pauseBtn = document.querySelector('.pause-btn');
+  // let darkModeBtn = document.querySelector('body header button');
+  // playBtn.setAttribute('style', 'border: 3px solid white');
+  // pauseBtn.setAttribute('style', 'border: 3px solid white');
+  // darkModeBtn.setAttribute('style', 'border: 3px solid white');
+}
